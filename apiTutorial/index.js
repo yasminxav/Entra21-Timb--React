b6fcf6a4d48a51a62fc6cf4d5b61f047c152f691
yasminxav <ygxavier.yx@gmail.com>
2021-08-26 11:52:03 -0300

    Servidor

--- apiTutorial/index.js
+++ apiTutorial/index.js
@@ -2,56 +2,68 @@ const { response } = require("express");
 const express = require("express");
 const app = express();
 const PORT = 3000;
+const userControlers = require("../controlers/usersControllers")
+
+
+//Importando as rotas 
+const usersRoutes = require("./routes/usersRoutes")
+
+//Definindo os middlewares
+app.use(express.json());
+
+//Defindo as rotas 
+app.use("/users", usersRoutes);
+
 
 app.get("/",(request, response) => {
     response.send("Hello World!");
 });
 
 app.post("/", (request, response) => {
-    response.send("Métodp POST");
+    response.send("Método POST");
 });
 
 app.put("/", () =>{
     response.send("Método PUT");
 });
 
-app.listen(PORT, () => console.log("O servidor está rodando..."))
 
 
-const users = [
-    { id: 1, name: "Pedro", email: "pedro@email.com" },
-    { id: 2, name: "João", email: "joao@email.com" },
-    { id: 3, name: "Marcos", email: "marcos@email.com" },
-    ];
-
-//Rotas de Usuarios "users"
-app.get("/users", (req, res) => {
-    res.json(users);
-});
 
 
-app.get("/users/:id", (req, res) => {
-    const user_id = req.params.id;
-    console.log(user_id);
-
-    const user = users.find(user => user.id === user_id);
-
-    if (!user) {
-    res.status(404).json({ message: "User not found!" });
-    }
-
-    res.json(user);
-});
 
 
-app.get("/users/:id", (req, res) => {
+    // Criar um usuário
+    app.post("/users", (req, res) => {
+    const { id, name, email } = req.body;
+    
+    const usersAlreadyExists = users.find(user => user.email === email) 
+        if(usersAlreadyExists) {
+            return res.status(409).json({message:"user already exists"})
+        }
+    
+     res.json(req.body);
+    });
+    
+    // Atualizar as informações de um usuário
+    app.put("/users", (req, res) => {
+    });
+    
+    // Remover um usuário
+    app.delete("/users/:id", (req, res) => {
+        // Obter o id dos parametros
     const userId = req.params.id;
-    
-     const user = users.find(user => user.id == userId);
-    
-     if (!user) {
-    res.status(404).json({ message: "User not found!" });
+        // Verificar se o usuario com aquele id existe
+    const userIdInDB = users.findIndex(user = user.id == userId);
+
+        if (userIdInDB < 0) {
+    return res.status(404).json({ message: "User not found" });
     }
-    
-     res.json(user);
-});
+
+    // Remover o usuario do bd ()
+    users.splice(userIdInDB, 1);
+
+    res.status(204).end();
+    });
+
+    app.listen(PORT, () => console.log("O servidor está rodando..."))
